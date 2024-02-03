45ae38126ecfe5b6f416a96a399ec34bb5b72f50
Kanika1497 <kanikakur14@gmail.com>
2024-02-04 00:03:09 +0530

    feat: added background img link for blog

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'blog-top': "url('https://images.pexels.com/photos/594226/pexels-photo-594226.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+      }
+    },
   },
   plugins: [],
 }
